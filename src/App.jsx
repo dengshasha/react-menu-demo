fee5efc0889dfb733b6407ebdf4de12130b6572c
xuelian <18780488731@163.com>
2025-02-27 16:35:20 -0800

    feat: Set CI/CD and Github Pages Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router'
 import MenuDetail from './components/MenuDetail'
 import Navbar from './components/Navbar'
